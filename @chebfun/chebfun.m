3a70ddcd1385bd780b3d225cbe1bcff801bfbdb7
kolmanthomas <realkolmanthomas@gmail.com>
2025-07-27 18:15:22 -0400

    Revert "octave: workaround subsref and property fighting over "domain""
    This reverts commit 45a8b00f67e740aa0ebe73ba4c375ab5bfa7b55e.

--- @chebfun/chebfun.m
+++ @chebfun/chebfun.m
@@ -136,7 +136,7 @@ classdef chebfun
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %% CLASS PROPERTIES:
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    properties (Access = private)
+    properties (Access = public)
         % DOMAIN of definition of a CHEBFUN object. If K = length(F.DOMAIN) is
         % greater than 1 then the CHEBFUN is referred to as a "piecewise".
         % CHEBFUN. The first and last values of this vector define the left and
@@ -144,9 +144,8 @@ classdef chebfun
         % locations of the interior breakpoints that define the domains of the
         % individual FUN objects comprising the CHEBFUN. The entries in this
         % vector should be strictly increasing.
-        mydomain              % (1x(K+1) double)
-    end
-    properties (Access = public)
+        domain              % (1x(K+1) double)
+
         % FUNS is a cell array containing the FUN objects that comprise a
         % piecewise CHEBFUN. The kth entry in this cell is the FUN defining
         % the representation used by the CHEBFUN object on the open interval
@@ -217,14 +216,14 @@ classdef chebfun
                 % Construct from function_handle, numeric, or string input:
                 
                 % Call the main constructor:
-                [f.funs, f.mydomain] = chebfun.constructor(op, dom, data, pref);
+                [f.funs, f.domain] = chebfun.constructor(op, dom, data, pref);
                 
                 if ( flags.doubleLength )
                     % Using the length of f.funs{1} is okay because the
                     % 'doubleLength' flag is mutually exclusive with 'splitting
                     % on'.
                     pref.techPrefs.fixedLength = 2*length(f.funs{1}) - 1;
-                    [f.funs, f.mydomain] = chebfun.constructor(op, dom, data, pref);
+                    [f.funs, f.domain] = chebfun.constructor(op, dom, data, pref);
                 end
 
                 % Update values at breakpoints (first row of f.pointValues):
