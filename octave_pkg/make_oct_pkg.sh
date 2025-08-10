239fa3dbe50ebb6fa45d116b3821063f8cabef5e
Colin B. Macdonald <cbm@m.fsf.org>
2025-08-09 22:42:31 -0700

    Rough script to prepare an Octave package

---
+++ octave_pkg/make_oct_pkg.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Very rough packaging script, many packages have a "Maintainers Makefile"
+# that does a much nicer job.  But this might do for now!
+
+A=chebfun-5.7.0+
+
+rm -rf $A
+mkdir $A
+cp NEWS DESCRIPTION $A
+cp ../LICENSE.txt $A/COPYING
+pushd $A
+mkdir inst
+cp -ra ../../ inst/
+rm -rf inst/.git
+rm -rf inst/.github
+popd
+tar zcvf $A.tar.gz $A
+
